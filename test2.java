a0ca4251ea77b9eb89ecfd97ee460d98ead3755e
Joaquin Montesano <70350164+joacomonte@users.noreply.github.com>
2022-05-15 22:24:46 -0300

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,82 +1,65 @@
-package Test;
-
+package test1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+public class test1 {
 
-public class test2
-{
-    public static void main(String[] args)
-    {
-        int a = 1;
-        int b = 1;
-        int c = 2;
-        int d = 1;
-        System.out.println(solution2(a,b,c,d));
+    public static void main(String[] args) {
+        clock();
     }
 
+    public static boolean checkAndDelete(List nums, int[] time) {
 
-    public static boolean solution2(int a, int b, int c, int d)
-    {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(a,b,c,d));
+        List<Integer> e = new ArrayList<>(nums);
 
-        int A = 0;
-        int B = 0;
-        int C = 0;
-        int D = -1;
-
-        int count=0;
-
-        while(true) {
-
-            if (A == 2 && B == 3 && C == 5 && D == 9) {
-                break;
-            } else {
-                D++;
-
-                if (D > 9) {
-                    D = 0;
-                    C++;
-                }
-                if (C > 5) {
-                    C = 0;
-                    B++;
-                }
-                if (B > 9) {
-                    B = 0;
-                    A++;
-                }
-                System.out.println(A + "" + B + ":" + C + "" + D);
-
-
-                List<Integer> e = new ArrayList<>(numbers);
-                while(true){
-                    for (int i = e.size() - 1 ; i >= 0 ; i--){
-                        if(e.get(i) == A) {
-                            e.remove(i);
-                            for (i = e.size() - 1; i >= 0; i--) {
-                                if (e.get(i) == B) {
-                                    e.remove(i);
-                                    for (i = e.size() - 1; i >= 0; i--) {
-                                        if (e.get(i) == C) {
-                                            e.remove(i);
-                                            for (i = e.size() - 1; i >= 0; i--) {
-                                                if (e.get(i) == D) {
-                                                    count = count + 1;
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+        for (int index = 0; index < time.length; index++) {
+            for (int i = e.size() - 1; i >= 0; i--) {
+                if (e.get(i).equals(time[index])) {
+                    e.remove(i);
+                    if (e.size() == 0) return true;
                     break;
                 }
             }
         }
-        System.out.println(count);
         return false;
     }
+
+
+
+    public static void clock() {
+
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 3, 3, 2));
+
+        int count=0;
+        int[] time = {0, 0, 0, -1};
+
+        while (true) {
+            if (time[0] == 2 && time[1] == 3 && time[2] == 5 && time[3] == 9) {
+                break;
+            } else {
+                time[3]++;
+
+                if (time[3] > 9) {
+                    time[3] = 0;
+                    time[2]++;
+                }
+                if (time[2] > 5) {
+                    time[2] = 0;
+                    time[1]++;
+                }
+                if (time[1] > 9) {
+                    time[1] = 0;
+                    time[0]++;
+                }
+//                System.out.println(time[0] + "" + time[1] + ":" + time[2] + "" + time[3]);
+                if(checkAndDelete(numbers,time)){
+                    count=count+1;
+                };
+            }
+        }
+        System.out.println(count);
+    }
 }
+
+
